36e17a5c450e23da9e66c6076d53ba8b0d214e44
sonup97 <sonupandit97@gmail.com>
2023-03-12 18:23:49 +0530

    first-ecomm-commit

---
+++ src/main/java/com/ytecomm/jwtecomm/JwtEcommApplication.java
@@ -0,0 +1,13 @@
+package com.ytecomm.jwtecomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtEcommApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtEcommApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ytecomm/jwtecomm/JwtEcommApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ytecomm.jwtecomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtEcommApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
