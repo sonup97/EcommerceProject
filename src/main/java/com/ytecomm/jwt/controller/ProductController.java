3d222878a339efc5edcd3565403a1b236fbe07ab
sonup97 <sonupandit97@gmail.com>
2023-05-03 21:54:00 +0530

    uploade image or browse image done

--- src/main/java/com/ytecomm/jwt/controller/ProductController.java
+++ src/main/java/com/ytecomm/jwt/controller/ProductController.java
@@ -1,12 +1,19 @@
 package com.ytecomm.jwt.controller;
 
 
+import com.ytecomm.jwt.entity.ImageModel;
 import com.ytecomm.jwt.entity.Product;
 import com.ytecomm.jwt.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 @RestController
 public class ProductController {
@@ -14,9 +21,32 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @PostMapping({"/addNewProduct"})
-    public Product addNewProduct(@RequestBody Product product){
+    @PostMapping(value = {"/addNewProduct"}, consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public Product addNewProduct(@RequestPart("product") Product product,
+                                 @RequestPart("imageFile") MultipartFile[] file){
+//        return productService.addNewProduct(product);
+        try{
+            Set<ImageModel> images = uploadImage(file);
+            product.setProductImages(images);
+            return productService.addNewProduct(product);
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            return null;
+        }
 
-        return productService.addNewProduct(product);
+    }
+    public Set<ImageModel> uploadImage(MultipartFile[] multipartFiles) throws IOException {
+        Set<ImageModel> imageModels = new HashSet<>();
+        for(MultipartFile file: multipartFiles){
+            ImageModel imageModel = new ImageModel(
+                    file.getOriginalFilename(),
+                    file.getContentType(),
+                    file.getBytes()
+            );
+            imageModels.add(imageModel);
+
+        }
+        return imageModels;
     }
 }
