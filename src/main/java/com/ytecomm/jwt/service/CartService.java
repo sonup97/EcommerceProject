6fb77d6de34900513c087d90a8bdfe110ce9a1a3
sonup97 <sonupandit97@gmail.com>
2023-07-16 21:49:12 +0530

    duplicate product in cart fixed

--- src/main/java/com/ytecomm/jwt/service/CartService.java
+++ src/main/java/com/ytecomm/jwt/service/CartService.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CartService {
@@ -31,6 +32,13 @@ public class CartService {
         User user = null;
         if(username != null) {
             user = userRepository.findById(username).get();
+        }
+
+        List<Cart> cartList = cartRepository.findByUser(user);
+        List<Cart> filteredList = cartList.stream().filter(x -> x.getProduct().getProductId() == productId).collect(Collectors.toList());
+
+        if(filteredList.size() > 0){
+            return null;
         }
         if(product != null && user != null){
             Cart cart = new Cart(product,user);
